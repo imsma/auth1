a1c7a2db3b9adac0815a3479d07314b3d1374dba
SMA <sma@wizlinx.com>
2025-01-25 22:04:58 +0500

    Add cookie authentication and implement login endpoint with claims

--- Program.cs
+++ Program.cs
@@ -1,17 +1,44 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+
+const string AuthSchme = "cookie";
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(AuthSchme)
+        .AddCookie(AuthSchme);
 
 
 var app = builder.Build();
 
-app.MapGet("/username", () =>
+app.UseAuthentication();
+
+app.MapGet("/login", async (HttpContext ctx) =>
 {
+
+    var claims = new List<Claim>
+    {
+        new Claim("usr", "sma")
+    };
+
+    var identity = new ClaimsIdentity(claims, AuthSchme);
+    var user = new ClaimsPrincipal(identity);
+
+    await ctx.SignInAsync(AuthSchme, user);
 
 });
 
-app.MapGet("/login", (HttpContext ctx) =>
+
+app.MapGet("/unsecure", (HttpContext ctx) =>
 {
+    var user = ctx.User;
+    var username = user.FindFirst("usr")?.Value ?? "Anonymous";
+
+    return username;
 
 });
+
+
 
 
 
