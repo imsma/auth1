895915e0f9edca37c04e397cc86ed31b817b63ba
SMA <sma@wizlinx.com>
2025-01-25 20:38:52 +0500

    Implement custom middleware for user claims extraction from auth cookie

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using auth1;
 using Microsoft.AspNetCore.DataProtection;
 
@@ -10,18 +11,34 @@ builder.Services.AddScoped<AuthService>();
 
 var app = builder.Build();
 
-
-
-app.MapGet("/username", (HttpContext ctx, IDataProtectionProvider idp) =>
+// Custom middleware
+app.Use((ctx, next) =>
 {
+    var idp = ctx.RequestServices.GetRequiredService<IDataProtectionProvider>();
     var protector = idp.CreateProtector("auth-cookie");
     // var authCookie = ctx.Request.Cookies["auth"];
     var authCookie = ctx.Request.Cookies.FirstOrDefault(c => c.Key == "auth");
     var auth = authCookie.Value;
     var unprotectedUserValue = protector.Unprotect(auth);
-    var user = unprotectedUserValue.Split(":")[1];
+    var key = unprotectedUserValue.Split(":")[0];
+    var value = unprotectedUserValue.Split(":")[1];
 
-    return user;
+    var claims = new List<Claim>();
+    claims.Add(new Claim(key, value));
+    var identity = new ClaimsIdentity(claims);
+    var principal = new ClaimsPrincipal(identity);
+
+    ctx.User = principal;
+
+    return next();
+});
+
+
+
+app.MapGet("/username", (HttpContext ctx) =>
+{
+
+    return ctx.User.FindFirst("usr")?.Value;
 });
 
 app.MapGet("/login", (AuthService authService) =>
